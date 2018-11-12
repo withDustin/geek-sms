09153d88b824cff0e37573efe9e4be30b3fad171
Dương Đỗ <dustin.do95@gmail.com>
2018-11-12 13:27:42 +0700

    chore: Update jest & tsconfig to optimize coverage

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   modulePaths: ['<rootDir>/src'],
   verbose: true,
+  testPathIgnorePatterns: ['/node_modules/', '/lib/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/lib/', 'config.ts'],
 }
